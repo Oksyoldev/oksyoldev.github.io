c7ccd7b30e3a34f5e6179fd5ca2bebfb13fa368e
Oksyoldev <bloger200317@gmail.com>
2024-03-12 14:46:30 +0300

    fix bags

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const links = document.querySelectorAll('a[href^="#"]');
+  
+  links.forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+      
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+});
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
