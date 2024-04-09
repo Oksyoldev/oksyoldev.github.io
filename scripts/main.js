ddf107df777d3c8fcb3be983b9ccd4727adc9318
Oksyoldev <bloger200317@gmail.com>
2024-04-09 17:59:35 +0300

    beats

--- scripts/main.js
+++ scripts/main.js
@@ -27,36 +27,50 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
-const playBtn = document.querySelector('.play-btn');
+
+const playBtns = document.querySelectorAll('.play-btn');
 const playPauseBtn = document.querySelector('.play-pause-btn');
 const audio = document.getElementById('audio');
-let isPlaying = false;
 
-playBtn.addEventListener('click', function() {
-  if (!isPlaying) {
-    audio.play();
-  } else {
-    audio.pause();
-  }
-  togglePlayButtons();
+document.addEventListener('DOMContentLoaded', function() {
+  const playBtns = document.querySelectorAll('.play-btn');
+
+  let currentAudio = null;
+  let currentPlayBtn = null;
+
+  playBtns.forEach(playBtn => {
+    playBtn.addEventListener('click', function() {
+      const audio = this.parentElement.querySelector('audio');
+
+      if (audio !== currentAudio) {
+        if (currentAudio) {
+          currentAudio.pause();
+          currentAudio.currentTime = 0; // Сброс времени трека до начала
+          currentPlayBtn.querySelector('.bi-pause-fill').style.display = 'none';
+          currentPlayBtn.querySelector('.bi-play-fill').style.display = 'inline-block';
+        }
+
+        currentAudio = audio;
+        currentPlayBtn = this;
+        currentPlayBtn.isPlay = false;
+      }
+
+      if (audio.paused) {
+        audio.play();
+        this.isPlay = true;
+        this.querySelector('.bi-play-fill').style.display = 'none';
+        this.querySelector('.bi-pause-fill').style.display = 'inline-block';
+      } else {
+        audio.pause();
+        this.isPlay = false;
+        this.querySelector('.bi-pause-fill').style.display = 'none';
+        this.querySelector('.bi-play-fill').style.display = 'inline-block';
+      }
+    });
+  });
 });
 
-playPauseBtn.addEventListener('click', function() {
-  if (!isPlaying) {
-    audio.play();
-  } else {
-    audio.pause();
-  }
-  togglePlayButtons();
-});
 
-function togglePlayButtons() {
-  isPlaying = !isPlaying;
-  playBtn.querySelector('.bi-play-fill').style.display = isPlaying ? 'none' : 'inline-block';
-  playBtn.querySelector('.bi-pause-fill').style.display = isPlaying ? 'inline-block' : 'none';
-  playPauseBtn.querySelector('.bi-play-fill').style.display = isPlaying ? 'none' : 'inline-block';
-  playPauseBtn.querySelector('.bi-pause-fill').style.display = isPlaying ? 'inline-block' : 'none';
-}
 
 
 
