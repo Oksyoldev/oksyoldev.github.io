fc0ccf85336ee4114e64ccf40fde6fc0d282186f
Oksyoldev <bloger200317@gmail.com>
2024-04-15 12:54:32 +0300

    off play beats

--- scripts/main.js
+++ scripts/main.js
@@ -28,49 +28,6 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
-const playBtns = document.querySelectorAll('.play-btn');
-const playPauseBtn = document.querySelector('.play-pause-btn');
-const audio = document.getElementById('audio');
- const volumeSlider = document.querySelector('.volume-slider');
-
-document.addEventListener('DOMContentLoaded', function() {
-  const playBtns = document.querySelectorAll('.play-btn');
-
-  let currentAudio = null;
-  let currentPlayBtn = null;
-
-  playBtns.forEach(playBtn => {
-    playBtn.addEventListener('click', function() {
-      const audio = this.parentElement.querySelector('audio');
-
-      if (audio !== currentAudio) {
-        if (currentAudio) {
-          currentAudio.pause();
-          currentAudio.currentTime = 0; // Сброс времени трека до начала
-          currentPlayBtn.querySelector('.bi-pause-fill').style.display = 'none';
-          currentPlayBtn.querySelector('.bi-play-fill').style.display = 'inline-block';
-        }
-
-        currentAudio = audio;
-        currentPlayBtn = this;
-        currentPlayBtn.isPlay = false;
-      }
-
-      if (audio.paused) {
-        audio.play();
-        this.isPlay = true;
-        this.querySelector('.bi-play-fill').style.display = 'none';
-        this.querySelector('.bi-pause-fill').style.display = 'inline-block';
-      } else {
-        audio.pause();
-        this.isPlay = false;
-        this.querySelector('.bi-pause-fill').style.display = 'none';
-        this.querySelector('.bi-play-fill').style.display = 'inline-block';
-      }
-    });
-  });
-});
-
 
 
 
